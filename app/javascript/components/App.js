f4760cff96a54684aaaabfbdea57786cd3053ba9
nelsinofrancisco <nelsino.francisco@gmail.com>
2022-01-22 13:26:56 -0300

    feature: finish hello react-rails app

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,16 +1,20 @@
 import React from "react"
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HelloWorld from './HelloWorld'
+import configureStore from "../redux/configureStore"
+import { Provider } from "react-redux"
 
+const store = configureStore();
 const App = () => {
-  
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route exact path="/" element={<>Home!</>}/>
-        <Route path="/hello" element={<HelloWorld greeting="Friend" />}/>
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<>Home!</>}/>
+          <Route path="/hello" element={<HelloWorld greeting="Friend" />}/>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   )
 }
 
--- app/javascript/components/HelloWorld.js
+++ app/javascript/components/HelloWorld.js
@@ -1,18 +1,53 @@
 import React from "react"
 import { PropTypes } from "prop-types"
+import { createStructuredSelector } from "reselect"
+import { connect } from "react-redux"
+import { v4 as uuidv4 } from 'uuid';
+
+const GET_THINGS_REQUEST = 'GET_THINGS_REQUEST'
+const GET_THINGS_SUCCESS = 'GET_THINGS_SUCCESS'
+
+const getThings = () => {
+  return dispatch => {
+    dispatch({ type: GET_THINGS_REQUEST })
+
+    return fetch(`v1/things.json`)
+      .then(response => response.json())
+      .then(json => dispatch(getThingsSuccess(json)))
+      .catch(error => console.log(error))
+  }
+}
 
 const HelloWorld = (props) => {
-  const { greeting } = props
+  const { greeting, things } = props
+  const thingsList = things.map((things) =>
+    <li key={uuidv4()}>{things.name} {things.guid}</li>
+  )
 
   return (
     <React.Fragment>
       Greeting: { greeting }
+      <button className='getThingsBtn' onClick={() => props.getThings()}>
+        getThings
+      </button>
+      <br/>
+      <ul>{ thingsList }</ul>
     </React.Fragment>
   )
 }
 
-HelloWorld.propTypes = {
-  greeting: PropTypes.string
-};
+const structuredSelector = createStructuredSelector({
+  things: state => state.things,
+})
 
-export default HelloWorld
+const mapDispatchToProps = { getThings }
+
+const getThingsSuccess = (json) => {
+  return {
+    type: GET_THINGS_SUCCESS,
+    json
+  }
+}
+
+export { getThingsSuccess }
+export default connect(structuredSelector, mapDispatchToProps)(HelloWorld)
